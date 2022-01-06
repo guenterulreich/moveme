fd5bd03687477cb2448c0109f838c4a0a71bc4af
Hans Sowa <hans.sowa@polygons.at>
2022-01-06 19:02:05 +0100

    Add pretty, json and html report
    Cucumber will report now in different formats. So, it is easier to see the benefits of such approach.

--- src/test/java/fh/burgenland/moveme/it/CucumberIntegrationTest.java
+++ src/test/java/fh/burgenland/moveme/it/CucumberIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
@@ -11,6 +12,10 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @ConfigurationParameter(
         key = GLUE_PROPERTY_NAME,
         value = "fh.burgenland.moveme.it"
+)
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty,json:build/cucumber.json,html:build/cucumber.html"
 )
 public class CucumberIntegrationTest {
 }
